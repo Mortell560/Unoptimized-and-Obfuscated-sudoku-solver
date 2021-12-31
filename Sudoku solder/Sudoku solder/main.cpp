1197301aa9ee288542bc4ee039cac5bec0c6e650
Mortell560 <mortell560@gmail.com>
2022-01-01 00:40:26 +0100

    Update main.cpp
    and again

--- Sudoku solder/Sudoku solder/main.cpp
+++ Sudoku solder/Sudoku solder/main.cpp
@@ -3,8 +3,8 @@
 #include "Board.hpp"
 
 int main(int argc, char** argv) {
-	auto board = sudoku::Board::Board(2 ,2 , 2);
-	sudoku::Board::generateRndProblem(board, 3);
+	auto board = sudoku::Board::Board(2 ,2 ,2);
+	sudoku::Board::generateRndProblem(board, 1);
 	sudoku::Board::showBoard(board);
 	sudoku::Board::solveProblemUnique(board);
 	return 0;
